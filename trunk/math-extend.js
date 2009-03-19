c35880d8df27146298cd13c51db05f5fb152c4a4
Laurent.Fortin <Laurent.Fortin@79ad02ce-304f-0410-8e23-ff7d444270a5>
2009-03-19 21:29:11 +0000

    Changes for version 0.5.0

--- trunk/math-extend.js
+++ trunk/math-extend.js
@@ -1,6 +1,6 @@
-// Math.extend v0.4.0
+// Math.extend v0.5.0
 
-// Copyright (c) 2008 Laurent Fortin
+// Copyright (c) 2008-2009 Laurent Fortin
 // 
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -23,124 +23,113 @@
 
 
 Object.extend(Math, {
-    
     sum: function(list) {
         var sum = 0;
         
-	for(var i = 0, len = list.length; i < len; i++) {
+	for(var i = 0, len = list.length; i < len; i++)
 	    sum += list[i];
-	}
 	
         return sum;
     },
+    
     mean: function(list) {
-        if(list.length) {
-            return this.sum(list) / list.length;
-        } else {
-            return false;
-        }
+        return list.length ? this.sum(list) / list.length : false;
     },
+    
     median: function(list) {
-        if(list.length) {
-            list = this.sort(list);
-            
-            if(list.length.isEven()) {
-                return this.mean([list[list.length / 2 - 1],list[list.length / 2]]);
-            } else {
-                return list[(list.length / 2).floor()];
-            }
+        if(!list.length) return false;
+        
+        list = this.sort(list);
+        
+        if(list.length.isEven()) {
+            return this.mean([list[list.length / 2 - 1], list[list.length / 2]]);
         } else {
-            return false;
+            return list[(list.length / 2).floor()];
         }
     },
+    
+    variance: function(list) {
+        if(!list.length) return false;
+        
+        var mean = this.mean(list);
+        var dev = [];
+        
+        for(var i = 0, len = list.length; i < len; i++)
+	    dev.push(this.pow(list[i] - mean, 2));
+        
+        return this.mean(dev);
+    },
+    
     stdDev: function(list) {
-        if(list.length) {
-            var mean = this.mean(list);
-            var dev = [];
-            
-	    for(var i = 0, len = list.length; i < len; i++) {
-		dev.push(this.pow(list[i] - mean, 2));
-	    }
-	    
-            return this.sqrt(this.mean(dev));
-        } else {
-            return false;
-        }
+    	return this.sqrt(this.variance(list));
     },
+    
     sort: function(list, desc) {
-        return list.sort(function(a,b){return(desc ? b - a : a - b)});
+        // we output a clone of the original array
+        return list.clone().sort(function(a, b) { return desc ? b - a : a - b });
     },
+    
     baseLog: function(n, base) {
         return this.log(n) / this.log(base || 10);
     },
+    
     factorize: function(n) {
-	if(!n.isNatural(true) || n == 1) {
-	    return false;
-	}
-	if(n.isPrime()) {
-	    return [n];
-	}
+	if(!n.isNatural(true) || n == 1) return false;
+	if(n.isPrime()) return [n];
 	
     	var sqrtOfN = this.sqrt(n);
     	
-	for(var i = 2; i <= sqrtOfN; i++) {
-	    if(i.isPrime() && (n / i).isInteger()) {
-	    	var result = [i];
-	    	
-	    	result.push(this.factorize(n / i));
-	    	
-	    	return(result.flatten());
-	    }
-	}
+        for(var i = 2; i <= sqrtOfN; i++)
+            if(i.isPrime() && (n / i).isInteger())
+                return [i, this.factorize(n / i)].flatten();
     },
+    
     sinh: function(n) {
         return (this.exp(n) - this.exp(-n)) / 2;
     },
+    
     cosh: function(n) {
         return (this.exp(n) + this.exp(-n)) / 2;
     },
+    
     tanh: function(n) {
         return this.sinh(n) / this.cosh(n);
     }
-
 });
 
 Object.extend(Number.prototype, {
-
     isNaN: function() {
 	return isNaN(this);
     },
+    
     isNull: function() {
 	return this == 0;
     },
+    
     isEven: function() {
-	if(!this.isInteger()) {
-	    return false;
-	}
+	if(!this.isInteger()) return false;
 	
         return(this % 2 ? false : true);
     },
+    
     isOdd: function() {
-	if(!this.isInteger()) {
-	    return false;
-	}
+	if(!this.isInteger()) return false;
 	
         return(this % 2 ? true : false);
     },
+    
     isInteger: function(excludeZero) {
     	// if this == NaN ...
-    	if(this.isNaN()) {
-    	    return false;
-    	}
-    	if(excludeZero && this == 0) {
-    	    return false;
-    	}
+    	if(this.isNaN()) return false;
+    	if(excludeZero && this == 0) return false;
     	
         return (this - this.floor()) ? false : true;
     },
+    
     isNatural: function(excludeZero) {
 	return(this.isInteger(excludeZero) && this >= 0);
     },
+    
     isPrime: function() {
 	var sqrtOfThis = Math.sqrt(this);
 	var somePrimes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101];
@@ -159,7 +148,7 @@ Object.extend(Number.prototype, {
 		return false;
 	    }
 	}
-	for(var i = 103; i <= sqrtOfThis; i++) {
+	for(var i = 103; i <= sqrtOfThis; i += 2) {
 	    if(this % i == 0) {
 		return false;
 	    }
@@ -167,14 +156,13 @@ Object.extend(Number.prototype, {
 	
 	return true;
     },
+    
     compute: function(fn) {
-        return (fn)(this);
+        return fn(this);
     }
-
 });
 
 Object.extend(Array.prototype, {
-
     swap: function(index1, index2) {
         var swap = this[index1];
         
@@ -183,15 +171,17 @@ Object.extend(Array.prototype, {
         
         return this;
     },
+    
     shuffle: function(inline, times) {
         var list = (inline != false ? this : this.clone());
         
         for(var i = 0, len = list.length * (times || 4); i < len; i++) {
-            list.swap((Math.random() * list.length).floor(),(Math.random() * list.length).floor());
+            list.swap((Math.random() * list.length).floor(), (Math.random() * list.length).floor());
         }
         
         return list;
     },
+    
     randomDraw: function(items) {
         items = Number(items) || 1;
         
@@ -213,23 +203,11 @@ Object.extend(Array.prototype, {
         
         return sample;
     }
-
 });
 
 Object.extend(Object, {
-
-    numericValues: function(obj) {
-        var objValues = Object.values(obj);
-        var objNumericValues = [];
-        
-        for (i = 0, len = objValues.length; i < len; i++) {
-            if (Object.isNumber(objValues[i])) {
-                objNumericValues.push(objValues[i]);
-            }
-        }
-        
-        return objNumericValues;
+    numericValues: function(object) {
+        return Object.values(object).select(Object.isNumber);
     }
-
 });
 
